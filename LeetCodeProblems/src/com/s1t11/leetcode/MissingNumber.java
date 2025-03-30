f9c801ee65c7ed97f084bf32f8d676a91cd0e472
aayoaj14 <aayoaj14@gmail.com>
2025-03-30 17:27:00 -0400

    Fixed-MissingNumber solution

---
+++ LeetCodeProblems/src/com/s1t11/leetcode/MissingNumber.java
@@ -0,0 +1,26 @@
+package com.s1t11.leetcode;
+
+public class MissingNumber {
+	public int missingNumber(int[] nums) {
+		// set a variable for sum
+		int sum = 0;
+		// set a variable for the expected sum
+		int expectedSum = 0;
+		// for loop that loops through the index of the array
+		for (int i = 0; i < nums.length; i++) {
+			// add sum of each element at each index
+			sum += nums[i];
+		}
+		// another for loop that loops through the length of the array +1
+		for (int i = 0; i < nums.length + 1; i++) {
+
+			// add every index together into expected sum
+			expectedSum += i;
+		}
+		// return expected sum - sum
+		return expectedSum - sum;
+	}
+
+	public static void main(String[] args) {
+	}
+}
